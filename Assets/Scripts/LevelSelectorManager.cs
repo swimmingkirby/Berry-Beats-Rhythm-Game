f4fb8adaa864f0bea36e482eb71ee0e6dda2ac38
Desmazio <ale.pau02@tiscali.it>
2021-09-09 15:31:06 +0200

    Added train bg

--- Assets/Scripts/LevelSelectorManager.cs
+++ Assets/Scripts/LevelSelectorManager.cs
@@ -19,6 +19,7 @@ public class LevelSelectorManager : MonoBehaviour
 
     int currentLevel = 0;
     int levelCount = 1;
+    int levelIndex = 0;
     Vector2 levelTextPosition;
     RectTransform[] backgroundObjects;
 
@@ -39,45 +40,58 @@ public class LevelSelectorManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            //levelIndex++;
             ChangeMenu(1);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            //levelIndex--;
             ChangeMenu(-1);
         }
     }
 
     public void ChangeMenu(int input)
     {
-        int _lastLevel = currentLevel;
-        currentLevel = Mathf.RoundToInt(Mathf.Repeat(currentLevel + input, levelCount));
-
-        // Cancel all other animations to prevent glitches
-        for (int i = 0; i < levelCount; i++)
+        if((input == 1 && levelIndex < 2) || (input == -1 && levelIndex > 0))
         {
-            LeanTween.cancel(backgroundObjects[i]);
+            if (input == 1)
+                levelIndex++;
+            else
+                levelIndex--;
+
+
+            int _lastLevel = currentLevel;
+            currentLevel = Mathf.RoundToInt(Mathf.Repeat(currentLevel + input, levelCount));
+
+            // Cancel all other animations to prevent glitches
+            for (int i = 0; i < levelCount; i++)
+            {
+                LeanTween.cancel(backgroundObjects[i]);
+            }
+            LeanTween.cancel(levelText.gameObject);
+
+            // The actual animations
+            LeanTween.alpha(backgroundObjects[_lastLevel], 0.0f, transitionDuration)
+                .setEase(transitionCurve);
+            LeanTween.alpha(backgroundObjects[currentLevel], 1.0f, transitionDuration)
+                .setEase(transitionCurve);
+            LeanTween.moveX(backgroundObjects[_lastLevel], input * -200, transitionDuration)
+                .setFrom(Vector3.zero)
+                .setEase(transitionCurve);
+            LeanTween.moveX(backgroundObjects[currentLevel], 0, transitionDuration)
+                .setFrom(200 * input * Vector3.right)
+                .setEase(transitionCurve);
+
+            LeanTween.moveLocalY(levelText.gameObject, levelTextPosition.y + 100, transitionDuration * 0.1f)
+                .setEaseOutQuint()
+                .setOnComplete(() => LeanTween.moveLocalY(levelText.gameObject, levelTextPosition.y, transitionDuration - 0.1f)
+                    .setEaseOutCirc());
+
+            print(levelIndex);
+            levelText.sprite = texts[levelIndex];
+            audioSource.clip = songs[levelIndex];
+            audioSource.Play();
         }
-        LeanTween.cancel(levelText.gameObject);
-
-        // The actual animations
-        LeanTween.alpha(backgroundObjects[_lastLevel], 0.0f, transitionDuration)
-            .setEase(transitionCurve);
-        LeanTween.alpha(backgroundObjects[currentLevel], 1.0f, transitionDuration)
-            .setEase(transitionCurve);
-        LeanTween.moveX(backgroundObjects[_lastLevel], input * -200, transitionDuration)
-            .setFrom(Vector3.zero)
-            .setEase(transitionCurve);
-        LeanTween.moveX(backgroundObjects[currentLevel], 0, transitionDuration)
-            .setFrom(200 * input * Vector3.right)
-            .setEase(transitionCurve);
-
-        LeanTween.moveLocalY(levelText.gameObject, levelTextPosition.y + 100, transitionDuration * 0.1f)
-            .setEaseOutQuint()
-            .setOnComplete(() => LeanTween.moveLocalY(levelText.gameObject, levelTextPosition.y, transitionDuration - 0.1f)
-                .setEaseOutCirc());
-
-        levelText.sprite = texts[currentLevel];
-        audioSource.clip = songs[currentLevel];
-        audioSource.Play();
+        
     }
 }
